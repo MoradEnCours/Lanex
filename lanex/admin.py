d4d9edf4237c1ee246280cb79ba9fb92013cdcab
Morad Elfleet <2465064e@student.gla.ac.uk>
2021-02-19 22:30:42 +0000

    Started testing with migrations, models and admin configurations

--- lanex/admin.py
+++ lanex/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from lanex.models import Language, UserProfile, LanguageRequest
 
-# Register your models here.
+
+admin.site.register(Language)
+admin.site.register(LanguageRequest)
+admin.site.register(UserProfile)
--- lanex/models.py
+++ lanex/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-from django.db import models
+
+class Language(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    picture = models.ImageField(upload_to='languages', default='languages/lan.jpg')
+
+    def __str__(self):
+        return self.name
 
 class UserProfile(models.Model):
     username = models.CharField(max_length=128, unique=True)
@@ -9,12 +15,14 @@ class UserProfile(models.Model):
     pageURL = models.CharField(max_length=128, unique=True)
     language = models.CharField(max_length=128, unique=True)
     location = models.CharField(max_length=128, unique=True)
-    social_media = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
         return self.username
         
 class LanguageRequest(models.Model):
+    title = models.CharField(max_length=128)
+    views = models.IntegerField(default=0)
+    request_id = models.CharField(max_length=128,primary_key=True)
     time = models.DateTimeField(auto_now=True)
     location = models.CharField(max_length=128, unique=True)
     sessionID = models.IntegerField(default=0)
