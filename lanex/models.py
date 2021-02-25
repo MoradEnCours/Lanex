7b786f7217c7f57df390a8a0516556d2323614fb
Morad Elfleet <2465064e@student.gla.ac.uk>
2021-02-25 19:09:33 +0000

    Updated the models file: had to erase previous migration; population script needs to be revised, along with forms file

--- lanex/models.py
+++ lanex/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from registration.signals import user_registered
+import uuid
 
 class Language(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
     picture = models.ImageField(upload_to='languages', default='languages/lan.jpg')
     
@@ -20,13 +20,21 @@ class Language(models.Model):
 class LanguageRequest(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
-    url = models.URLField()
     views = models.IntegerField(default=0)
-    ### Je dois repenser ceci-dessus
-    #request_id = models.CharField(max_length=128,primary_key=True)
-    #time = models.DateTimeField(auto_now=True)
-    #location = models.CharField(max_length=128, unique=True)
-    #sessionID = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    request_id = models.CharField(max_length=128,primary_key=True)
+    suggested_date = models.DateField(null=True, blank=True, help_text='MM/DD/YY (optional HH:MM)')
+    location = models.CharField(max_length=512)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    desc = models.CharField(max_length=1024)
+    completed = models.BooleanField(default=False)
+    picture = models.ImageField(upload_to='requests', blank=True)
+
+    def save(self, *args, **kwargs):
+        self.request_id = str(uuid.uuid4().int)
+        #request_id = models.CharField(max_length=128,primary_key=True)
+        self.slug = slugify(self.request_id)
+        super(LanguageRequest, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -45,5 +53,21 @@ class UserProfile(models.Model):
         return self.user.username
 
         
+class Comment(models.Model):
+    request = models.ForeignKey(LanguageRequest,on_delete=models.CASCADE,related_name='comments')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
 
 
+def createUserProfile(sender, user, request, **kwargs):
+    UserProfile.objects.get_or_create(user=user)
+    
+user_registered.connect(createUserProfile)
