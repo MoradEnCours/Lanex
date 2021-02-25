e907119242a56835b60369e68b50639b6315ab9f
Morad Elfleet <2465064e@student.gla.ac.uk>
2021-02-25 21:48:15 +0000

    Mofidied forms file: should check at some point also on modifying templates and learning further about how to make better use of JS, CSS, and the likes to ramp up on aesthetics of the pages

--- lanex/forms.py
+++ lanex/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
-from lanex.models import Language, LanguageRequest, UserProfile
+from django.contrib.auth.forms import UserChangeForm
+from lanex.models import Language, LanguageRequest, UserProfile, Comment
 
 class LanguageForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please add the name of the language.")
-    #views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    #likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
@@ -14,27 +13,45 @@ class LanguageForm(forms.ModelForm):
 
 class RequestForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please include the title.")
-    url = forms.URLField(max_length=200, help_text="Please input the URL.")
+    desc = forms.CharField(widget=forms.Textarea)
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    suggested_date = forms.DateTimeField(widget=forms.SelectDateWidget(), required=False)
+    city = forms.CharField(max_length=255, help_text="Search Map", initial="Glasgow")
 
     class Meta:
         model = LanguageRequest
-        exclude = ('language',)
+        exclude = ('language', 'slug', 'request_id','completed')
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-        if url and not url.startswith('http://'):
-            url = f'http://{url}'
-            cleaned_data['url'] = url
         return cleaned_data
+
+    field_order = ['language','title', 'desc', 'views','suggested_date','city','picture']
+
+class LanguageRequestForm(forms.ModelForm):
+    title = forms.CharField(max_length=128)
+    desc = forms.CharField(widget=forms.Textarea)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    suggested_date = forms.DateTimeField(widget=forms.SelectDateWidget(), required=False)
+    city = forms.CharField(max_length=255, help_text="Search Map", initial="Glasgow")
+
+    class Meta:
+        model = LanguageRequest
+        exclude = ('creator','slug','language','request_id','completed')
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        return cleaned_data
+
+    field_order = ['title', 'desc', 'views','suggested_date','city','picture']
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget = forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
 
 class UserForm2(forms.ModelForm):
 
@@ -47,3 +64,7 @@ class UserProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ('picture',)
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
