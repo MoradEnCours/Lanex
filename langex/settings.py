d46e89c3613ada6064beb110179f287bf4b0d5f4
Juraj Bublinec <jurajbublinec@gmail.com>
2021-03-17 17:39:25 +0100

    chore: add livereload server

--- langex/settings.py
+++ langex/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
 	'lanex',
     'registration',
@@ -67,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'langex.urls'
